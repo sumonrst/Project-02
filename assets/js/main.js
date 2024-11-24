b8fde8f9b3226b4801d53ca05788c530589cc592
sumonrst <eng.sumonrejata@gmail.com>
2024-11-24 14:49:00 +0600

    slider add done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function(){
+    $('.banner_area').slick({
+        // autoplay: true,
+        prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-right"></i></button>',
+        nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-arrow-left"></i></button>',
+    });
+  });
