295327136f27d3c3adf7f2a3c66f6309290804e9
sumonrst <eng.sumonrejata@gmail.com>
2024-11-25 10:26:44 +0600

    Testimonial section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,10 @@ $(document).ready(function(){
         prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-right"></i></button>',
         nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-arrow-left"></i></button>',
     });
+    $('.testimonial').slick({
+        autoplay: true,
+        slidesToShow:2,
+        arrows: false,
+        dots:true,
+    });
   });
