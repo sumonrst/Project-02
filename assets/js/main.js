afc65af383130ad3d2abb5d703a6cc89f8c25f81
sumonrst <eng.sumonrejata@gmail.com>
2024-11-25 06:48:56 +0600

    service section add

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@
 $(document).ready(function(){
     $('.banner_area').slick({
         autoplay: true,
-        infinite: true,
-        loop: true,
+        // infinite: true,
+        // loop: true,
         prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-right"></i></button>',
         nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-arrow-left"></i></button>',
     });
